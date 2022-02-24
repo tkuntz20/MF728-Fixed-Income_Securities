0be1096059ee29d3a090ecf4005daf77c3a962a7
tkuntz20 <thomaskuntz20@gmail.com>
2022-02-24 17:33:24 -0500

    Create MF728-HW-4.py

---
+++ .py Files/MF728-HW-4.py
@@ -0,0 +1,14 @@
+"""
+Created on 02/23/2022
+
+@author: Thomas Kuntz MF728-HW-4
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from math import *
+import scipy as sp
+from sympy.stats import Normal, cdf
+import scipy.stats as si
+from scipy.optimize import root
