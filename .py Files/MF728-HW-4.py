cd641f019bfbf9565a39caa577bc950ba904e71f
tkuntz20 <thomaskuntz20@gmail.com>
2022-03-31 23:19:57 -0400

    update

--- .py Files/MF728-HW-4.py
+++ .py Files/MF728-HW-4.py
@@ -73,19 +73,20 @@ if __name__ == '__main__':      # ++++++++++++++++++++++++++++++++++++++++++++++
 
     for i in range(len(K)):
         for j in range(len(K[0])):
-            premium[i][j] = bachelier(annuity[i], sigma[i][j] * bps, 5, F0[i] * bps, K[i][j] * bps)
+            #  bachelier(F, K, T, sigma, annuity):
+            premium[i][j] = bachelier(F0[i] * bps, K[i][j] * bps, 5, sigma[i][j] * bps, annuity[i])
     prem = pd.DataFrame(premium, index=['1Y','2Y','3Y','4Y','5Y'],columns=['ATM-50','ATM-25','ATM-5','ATM+5','ATM+25','ATM+50'])
-    print(f'\nPremiums: \n{prem}')
+    print(f'\nPremiums: \n{prem}\n')
 
     start = [0.1, 0.1, -0.1]
     param = np.zeros((5,3))
     for i in range(len(K)):
         opt = minimize(objectFunction, start, args=(5, K[i] * bps, F0[i] * bps, sigma[i] * bps), method = 'SLSQP', bounds = ((0.01,1.5),(0,1.5),(-1,1)))
         param[i] = opt.x
-        print(opt.fun)
+        print(f'minimized vol: \n{opt.fun}')
 
     SABR = pd.DataFrame(param, index = ['1Y','2Y','3Y','4Y','5Y'], columns = ['sigma0','alpha','rho'])
-    print(f'\nParameters : \n{SABR}')
+    print(f'\nParameters : \n{SABR}\n')
 
     F2 = np.array(2 * [117.45, 120.60, 133.03, 152.05, 171.85]).reshape(2, 5).T
     change2 = np.array([-75, 75])
@@ -95,12 +96,14 @@ if __name__ == '__main__':      # ++++++++++++++++++++++++++++++++++++++++++++++
 
     for i in range(len(K2)):
         for j in range(len(K2[0])):
-            sigma2[i][j] = asymp(5, K2[i][j] * bps, F0[i] * bps, param[i][0], param[i][1], 0.5, param[i][2])
+            #              asymp(F, K, T, sigma0, alpha, beta, rho)
+            sigma2[i][j] = asymp(F0[i] * bps, K2[i][j] * bps, 5, param[i][0], param[i][1], 0.5, param[i][2])
 
     K2 = F2 - change2
     for i in range(len(K2)):
         for j in range(len(K2[0])):
-            bachelier2[i][j] = bachelier(annuity[i], sigma2[i][j] * bps, 5, F0[i] * bps, K2[i][j] * bps)
+            #                   bachelier(F, K, T, sigma, annuity):
+            bachelier2[i][j] = bachelier(F0[i] * bps, K2[i][j] * bps, 5, sigma2[i][j] * bps, annuity[i])
     sig2 = pd.DataFrame(sigma2, index=['1Y', '2Y', '3Y', '4Y', '5Y'], columns=['ATM+75', 'ATM-75'])
     price2 = pd.DataFrame(bachelier2, index=['1Y', '2Y', '3Y', '4Y', '5Y'], columns=['ATM+75', 'ATM-75'])
     print(f"Normal Vols are:\n{sig2}\nPrices are:\n{price2}")
@@ -118,7 +121,7 @@ if __name__ == '__main__':      # ++++++++++++++++++++++++++++++++++++++++++++++
         for j in range(len(K[0])):
             bsdelta[i][j] = blackScholes(F0[i] * bps, K[i][j] * bps, 5, bsvol[i][j], annuity[i])[1]
     bsdel = pd.DataFrame(bsdelta, index=['1Y', '2Y', '3Y', '4Y', '5Y'], columns=['ATM-50', 'ATM-25', 'ATM-5', 'ATM+5', 'ATM+25', 'ATM+50'])
-    print(f"BS Vols are:\n{bsdel}")
+    print(f"BS deltas are:\n{bsdel}")
 
 
 
